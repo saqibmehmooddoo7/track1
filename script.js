8a318fcb79edd52c3a748bb8eb88b0d6c0b91f90
saqibmehmooddoo7 <saqibmehmooddoo7@gmail.com>
2024-08-27 17:11:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,8 @@
               'Content-Type': 'application/json'
           },
           body: JSON.stringify(trackingData),
-        credentials: 'include'
+        credentials: 'include',
+         mode: 'no-cors'
       })
       .then(response => {
           if (!response.ok) {
